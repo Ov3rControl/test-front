787d4c590058f67afd46f5e927555fb31148882f
Mohamed Adel <mohamedibrahimrc@gmail.com>
2020-11-20 17:19:37 +0200

    :package: Add Recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import "./App.css";
+import { Protected, routes } from "./routes";
+import ErrorBoundary from "./sharedComponents/ErrorBoundary";
+
+const mainRoutes = routes.map((route, index) => {
+  return (
+    <Protected
+      key={index}
+      name={route.name}
+      exact={route.exact}
+      path={route.path}
+      component={route.component}
+      requestedRole={route.requestedRole}
+    />
+  );
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RecoilRoot>
+        <BrowserRouter>
+          <ErrorBoundary>
+            <Switch>{mainRoutes}</Switch>
+          </ErrorBoundary>
+        </BrowserRouter>
+      </RecoilRoot>
+    </div>
+  );
 }
 
 export default App;
