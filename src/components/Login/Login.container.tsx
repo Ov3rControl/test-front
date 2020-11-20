20ed4b7c12a85e0a8435a89991c3873dfeb81e0f
Mohamed Adel <mohamedibrahimrc@gmail.com>
2020-11-20 11:55:05 +0200

    :sparkles: Add Login Component

--- src/components/Login/Login.container.tsx
+++ src/components/Login/Login.container.tsx
@@ -1,10 +1,23 @@
+import { AxiosResponse } from "axios";
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { LoginFormType } from "../../types";
+import api from "../../utils/api";
 import { LoginView } from "./Login.view";
 
 export const Login = () => {
-  return (
-    <div>
-      <LoginView />
-    </div>
-  );
+  const history = useHistory();
+  const onFinish = (value: LoginFormType) => {
+    api.postData("/auth", value, "POST").then((res: AxiosResponse) => {
+      if (res.data.auth) {
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("role", res.data.role);
+        res.data.role === "user"
+          ? history.push("/home")
+          : history.push("/dashboard");
+      }
+    });
+  };
+
+  return <LoginView onFinish={onFinish} />;
 };
--- src/components/Login/Login.view.tsx
+++ src/components/Login/Login.view.tsx
@@ -1,9 +1,40 @@
 import React, { FunctionComponent } from "react";
+import { Form, Input, Button } from "antd";
+import styles from "./Login.view.module.css";
+import { LoginFormType } from "../../types";
 
-export const LoginView: FunctionComponent = (): JSX.Element => {
+type Props = {
+  onFinish: (values: LoginFormType) => void;
+};
+
+export const LoginView: FunctionComponent<Props> = ({
+  onFinish,
+}: Props): JSX.Element => {
   return (
-    <div>
-      <p>Login View</p>
+    <div className={styles.Container}>
+      <Form name="basic" initialValues={{ remember: true }} onFinish={onFinish}>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
