9c987e44d65be0e25369b1d5621915e541014217
Mohamed Adel <mohamedibrahimrc@gmail.com>
2020-11-20 10:08:30 +0200

    :sparkles: Add Initial Login Component

---
+++ src/components/Login/Login.view.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+export const LoginView: FunctionComponent = (): JSX.Element => {
+  return (
+    <div>
+      <p>Login View</p>
+    </div>
+  );
+};
