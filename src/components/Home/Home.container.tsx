dfdb2fc984d2ad64f6f3d658917f5da6e35e8978
Mohamed Adel <mohamedibrahimrc@gmail.com>
2020-11-20 10:08:49 +0200

    :sparkles: Add Initial Home Component

---
+++ src/components/Home/Home.container.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HomeView } from "./Home.view";
+
+export const Home = () => {
+  return (
+    <div>
+      <HomeView />
+    </div>
+  );
+};
---
+++ src/components/Home/Home.view.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+export const HomeView: FunctionComponent = (): JSX.Element => {
+  return (
+    <div>
+      <p>Home View</p>
+    </div>
+  );
+};
